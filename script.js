0714d26bb312f62171cfccdb4cc9df9d82aafff1
gustavofloriano11 <gustavofloriano1@estudante.sesisenai.org.br>
2024-07-08 19:58:54 -0300

    adicionei o menu suspenso, agora ao clicar no icone do zap, ele redireciona para a conversa tbm.

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ function Whatsapp(){
     let numeroZap = "5547992606543"
     let urlZap = "https://wa.me/" + numeroZap
     window.open(urlZap, "_blank")
-}
+};
+
+let zap = document.getElementById("icone_zap");
+let icone = document.getElementById("icone_menu");
+let menu = document.getElementById("menu")
+menu.style.transform = "translateY(-101%)"
+
+icone.addEventListener("click", function(){
+    if (menu.style.transform == "translateY(0%)"){
+        console.log("Tá abrindo")
+        menu.style.transform = "translateY(-101%)"
+    } else {
+        console.log("Tá fechando")
+        menu.style.transform = "translateY(0%)"
+    }
+});
+
+zap.addEventListener("click", Whatsapp)
