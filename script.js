c5750d61b6356ed077ea723e6919222af63f942a
gustavofloriano11 <gustavofloriano1@estudante.sesisenai.org.br>
2024-07-18 22:56:23 -0300

    bem amigos, acabou pt final real oficial

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Whatsapp(){
-    let numeroZap = "5547997936232  "
+    let numeroZap = "5547997936232"
     let urlZap = "https://wa.me/" + numeroZap
     window.open(urlZap, "_blank")
 };
@@ -7,14 +7,7 @@ function Whatsapp(){
 let zap = document.getElementById("icone_zap");
 let icone = document.getElementById("icone_menu");
 let menu = document.getElementById("menu")
-menu.style.transform = "translateY(-101%)"
-
-icone.addEventListener("click", function(){
-    if (menu.style.transform == "translateY(0%)"){
-        menu.style.transform = "translateY(-101%)"
-    } else {
-        menu.style.transform = "translateY(0%)"
-    }
-});
+let zapDenovo = document.getElementById("icone_zap_fixado")
 
 zap.addEventListener("click", Whatsapp)
+zapDenovo.addEventListener("click", Whatsapp)
